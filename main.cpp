5524afb171820050f3aa36ba2c6eeab27f424ed2
Antonio <78964462+antonioalfaroa@users.noreply.github.com>
2022-08-10 06:07:28 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+  int n=10;
+  cout<<"In decimal = "<<dec<<n<<endl;
+  cout<<"In octal = "<<oct<<n<<endl;
+  cout<<"In hexadecimal = "<<hex<<n<<endl;
+}
